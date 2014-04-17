c6cd3b60d335dc74bd769f5d9047c752a7c7a5e3
Alex Forey <me@alexforey.com>
2014-04-17 09:57:02 +0100

    New git version

--- lib/arpm/cli.rb
+++ lib/arpm/cli.rb
@@ -9,56 +9,52 @@ module ARPM
       # Install all the things
       puts "Searching for packages...".cyan
 
-      data = URI.parse("https://raw.githubusercontent.com/alfo/arpm-test/master/packages.json").read
+      puts package = ARPM::Package.search(name)
 
-      packages = JSON.parse(data)
-
-      package = packages.select { |p| p['name'] == name }
-
-      if package.any?
-
-        path = ARPM::Config.base_directory + name
-
-        path = path + "_#{version.gsub('.', '_')}" if version
-
-        puts "Cloning #{name} into #{path}"
-
-        FileUtils.rm_r(path) if File.exists?(path)
-        repo = Git.clone(package.first["repository"], path)
-
-        tags = []
-        repo.tags.each { |t| tags << t.name }
-
-        puts "No releases of #{name} yet".red and return unless tags.any?
-
-        if (version)
-
-          if tags.include?(version)
-            repo.checkout("tags/#{version}")
-            puts "Installed #{name} version #{version}".green.bold
-          else
-            FileUtils.rm_r(path)
-            puts "#{name} version #{version} does not exist".red
-          end
-
-        else
-
-          tags.sort! { |x,y| y <=> x }
-          tags.each { |t| tags.pop(t) unless t[0].is_number? }
-
-          version = tags.first
-
-          repo.checkout("tags/#{version}")
-
-          FileUtils.mv(path, "#{path}_#{version.gsub('.', '_')}")
-
-          puts "Installed #{name} Version #{version}".green.bold
-
-        end
-
-      else
-        puts "No package named #{name} found".red
-      end
+      # if package?
+      #
+      #   path = ARPM::Config.base_directory + name
+      #
+      #   path = path + "_#{version.gsub('.', '_')}" if version
+      #
+      #   puts "Cloning #{name} into #{path}"
+      #
+      #   FileUtils.rm_r(path) if File.exists?(path)
+      #   repo = Git.clone(package.first["repository"], path)
+      #
+      #   tags = []
+      #   repo.tags.each { |t| tags << t.name }
+      #
+      #   puts "No releases of #{name} yet".red and return unless tags.any?
+      #
+      #   if (version)
+      #
+      #     if tags.include?(version)
+      #       repo.checkout("tags/#{version}")
+      #       puts "Installed #{name} version #{version}".green.bold
+      #     else
+      #       FileUtils.rm_r(path)
+      #       puts "#{name} version #{version} does not exist".red
+      #     end
+      #
+      #   else
+      #
+      #     tags.sort! { |x,y| y <=> x }
+      #     tags.each { |t| tags.pop(t) unless t[0].is_number? }
+      #
+      #     version = tags.first
+      #
+      #     repo.checkout("tags/#{version}")
+      #
+      #     FileUtils.mv(path, "#{path}_#{version.gsub('.', '_')}")
+      #
+      #     puts "Installed #{name} Version #{version}".green.bold
+      #
+      #   end
+      #
+      # else
+      #   puts "No package named #{name} found".red
+      # end
 
     end
 
